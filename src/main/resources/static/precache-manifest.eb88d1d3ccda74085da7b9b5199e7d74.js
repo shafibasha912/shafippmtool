33cab5de98f194e2494c0f22c6b2fe593479eea6
shafibasha912 <shafishaik.5a2@gmail.com>
2019-03-20 03:44:40 +0530

    react

---
+++ src/main/resources/static/precache-manifest.eb88d1d3ccda74085da7b9b5199e7d74.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "f8653efdc1a9e4c3e529",
+    "url": "/static/js/main.f8653efd.chunk.js"
+  },
+  {
+    "revision": "15a12ab560d06b11869e",
+    "url": "/static/js/2.15a12ab5.chunk.js"
+  },
+  {
+    "revision": "f8653efdc1a9e4c3e529",
+    "url": "/static/css/main.5f466c89.chunk.css"
+  },
+  {
+    "revision": "15a12ab560d06b11869e",
+    "url": "/static/css/2.0dda9e07.chunk.css"
+  },
+  {
+    "revision": "0c4e127cf48ae3fb807ebc4ed2c550bb",
+    "url": "/index.html"
+  }
+];
